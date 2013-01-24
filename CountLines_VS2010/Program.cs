e37a2f2060c15a962f6b167338770dcf0741e97b
fmj <fmj200x@gmail.com>
2013-01-24 08:33:47 +0100

    init

---
+++ CountLines_VS2010/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace CountLines_VS2010
+{
+    class Program
+    {
+        private static List<Markers> _tokens = new List<Markers>()
+            {
+                new Markers() {Start = "//", End = "\r\n"},
+                new Markers() {Start = "/*", End = "*/"},
+                new Markers() {Start = "\"", End = "\""}
+            };
+
+        static void Main(string[] args)
+        {
+            string input = File.ReadAllText(@"D:\Projects\CodingDOJO\CountLines_VS2010\TestFiles\TextFile1.txt");
+
+            StringBuilder output = new StringBuilder();
+            while (input.Length > 0)
+            {
+                foreach(Markers m in _tokens)
+                    m.Match(input);
+                _tokens.f
+
+            }
+
+        }
+    }
+
+    enum state
+    {
+        InMarker,Text
+    }
+
+    class Markers
+    {
+        public string Start { get; set; }
+        public string End { get; set; }
+        public state State { get; set; }
+        public int match{ get; set; }
+
+        public void Match(string input)
+        {
+            this.match =  input.IndexOf(Start);
+        }
+    }
+
+}
